cae14bed6cf4ad73b2afc91e4e3865ca1630abc6
Mitpf <mitfreex@gmail.com>
2023-03-21 11:22:02 +0200

    make http requester partial applicated for all methods

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -1,11 +1,11 @@
 
 
-import { request } from './requester'
+import * as request from './requester'
 const baseUrl = 'http://127.0.0.1:3030/jsonstore/games';
 
 export const getAll = async () => {
 
-    const result = await request('GET', baseUrl);
+    const result = await request.get(baseUrl);
     const games = Object.values(result)
     
 
